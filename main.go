c3535df8e706859447df37ec51b997b1d3247572
Dev Tuna <missiona.carla@gmail.com>
2020-07-14 16:22:19 +0800

    log s

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 var db *sql.DB
 
 func init() {
-	tmpDB, err := sql.Open("postgres", "dbname=d9fbdugs64n2l user=fcirxmuauuolmn password=505e9b87e24f6c1a647682111d98337e4f2f01a46e75b3cbf09cd3123e244ea8 host=ec2-3-215-83-17.compute-1.amazonaws.com sslmode=disable")
+	// db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// tmpDB, err := sql.Open("postgres", "dbname=d9fbdugs64n2l user=fcirxmuauuolmn password=505e9b87e24f6c1a647682111d98337e4f2f01a46e75b3cbf09cd3123e244ea8 host=ec2-3-215-83-17.compute-1.amazonaws.com sslmode=disable")
+	tmpDB, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
