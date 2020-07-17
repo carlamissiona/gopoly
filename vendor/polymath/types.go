38e92d2c68ff7270e731ec96eadb3ebdf1d9afb4
Dev Tuna <missiona.carla@gmail.com>
2020-07-17 12:37:25 +0800

    import

--- vendor/polymath/types.go
+++ vendor/polymath/types.go
@@ -1,7 +1,14 @@
 package polymath
 
-import "time"
+// import _ "time"
 
+type Book struct {
+	ID              int
+	Name            string
+	Author          string
+	PublicationDate time.Time
+	Pages           int
+}
 type HomePage struct {
 	AllBooks []Book
 	// AllCourses []Course
