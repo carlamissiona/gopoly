799dd6608715137b7d052a542c9afa2fbd73daf4
Dev Tuna <missiona.carla@gmail.com>
2020-07-17 13:57:15 +0800

    importing package ...

--- models/courses.go
+++ models/courses.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 	"github.com/lib/pq"
-	"polymath_types"
+	"polyweb"
 )
 
 //
--- vendor/polyweb/polytypes.go
+++ vendor/polyweb/polytypes.go
@@ -13,3 +13,22 @@ type HomePage struct {
 	AllBooks []Book
 	// AllCourses []Course
 }
+
+
+type Course struct {
+	ID              int
+	Name            string
+	Author          string
+	Tag             string
+	Category        string
+	Partners        string
+	Difficulty      string
+	Intro           string
+	NoOfStudents    int
+	Price           float32
+	IsFree          int
+	PassingScore    int
+	HoursToComplete int
+	PublicationDate time.Time
+	Pages           int
+}
